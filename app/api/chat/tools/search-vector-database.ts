2a37be5022568c442770994521c2f5172a38cb22
Farouk Charkas <farouk.charkas@gmail.com>
2025-11-24 18:43:53 +0530

    hyde

--- app/api/chat/tools/search-vector-database.ts
+++ app/api/chat/tools/search-vector-database.ts
@@ -5,7 +5,7 @@ import { searchPinecone } from "@/lib/pinecone";
 export const vectorDatabaseSearch = tool({
     description: 'Search the vector database for information',
     inputSchema: z.object({
-        query: z.string().describe('The query to search the vector database for'),
+        query: z.string().describe('The query to search the vector database for. Optimally is a hypothetical answer for similarity search.'),
     }),
     execute: async ({ query }) => {
         return await searchPinecone(query);
